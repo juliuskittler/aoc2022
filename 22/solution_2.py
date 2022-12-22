9b5e853c6a2c497a8e5977c52fd6a75aa7c430c9
juliuskittler <kittler.julius.uni@gmail.com>
2022-12-22 21:32:06 +0100

    Update 22

--- 22/solution_2.py
+++ 22/solution_2.py
@@ -1,10 +1,13 @@
 import pathlib
 from typing import List, Union
+
 from utils import get_instructions, get_map
 
 """Note: 
-- Take a look at the visualizations for example_2.pdf. 
-- The following algorithm can be used:
+- The following algorithm scetch could potentially be used (illustrated in example_2.pdf). 
+- However, I did not take the time to implement it in a way that generalizes to 
+different cube shapes. Instead, I kind of cheated and hard-coded the mapping.
+- The idea for an algorithm I would use if I had to implement it in a generalizable way:
 - 1. We start by mapping edges that are connected at 1 point and have a 90 degree angle 
 between them.
 - 2. We proceeed by mapping edges that point in the same direction (e.g. both right, 
@@ -14,16 +17,12 @@ other in some way (during step 1 or step 2.)
 or left and right) and have all edges between them already connected with each other 
 in some way (during step 1 or step 2.)
 - 4. Connect the remaining sides to the other sides that are closest to them.
-- How exactly do we map the edges? Each edge is a list of coordinates essentially. When
-we map two edges, we essentially put 2 lists on top of each other. Importantly, we need
-to consider the order of these lists:
-    - If the 2 sides point in opposite directions, then the order of both lists is aligned.
-    - If the 2 sides point in the same direction, then one of the 2 lists needs to be reverted.
-    - If the 2 sides point in different directions, then we map the transpose of one of the lists
-    with the other.
 """
 
-def solution(map: List[List[str]], instructions: List[Union[str, int]], verbose: bool = False) -> int:
+
+def solution(
+    map: List[List[str]], instructions: List[Union[str, int]], verbose: bool = False
+) -> int:
 
     # Initialize dimenions
     n_rows = len(map)
@@ -32,73 +31,95 @@ def solution(map: List[List[str]], instructions: List[Union[str, int]], verbose:
     # Initialize mapper to get the new direction
     new_direction = {
         "r": {
-            "L": "u", # if we look right, turn counter-clockwise, we look up (u)
-            "R": "d", # if we look right, turn clockwise, we look down (d)
-        }, 
+            "L": "u",  # if we look right, turn counter-clockwise, we look up (u)
+            "R": "d",  # if we look right, turn clockwise, we look down (d)
+        },
         "d": {
-            "L": "r", # if we look down, turn counter-clockwise, we look right (r)
-            "R": "l", # if we look down, turn clockwise, we look left (l)
-        }, 
+            "L": "r",  # if we look down, turn counter-clockwise, we look right (r)
+            "R": "l",  # if we look down, turn clockwise, we look left (l)
+        },
         "l": {
-            "L": "d", # if we look left, turn counter-clockwise, we look down (d)
-            "R": "u", # if we look left, turn clockwise, we look up ()
-        }, 
+            "L": "d",  # if we look left, turn counter-clockwise, we look down (d)
+            "R": "u",  # if we look left, turn clockwise, we look up ()
+        },
         "u": {
-            "L": "l", # if we look up, turn counter-clockwise, we look left (l)
-            "R": "r", # if we look up, turn clockwise, we look right (r)
-        }, 
+            "L": "l",  # if we look up, turn counter-clockwise, we look left (l)
+            "R": "r",  # if we look up, turn clockwise, we look right (r)
+        },
     }
 
     # Initialize mapper to change x position depending on the direction
-    new_x_delta = {
-        "u": 0,
-        "d": 0,
-        "r": 1,
-        "l": -1
-    }
+    new_x_delta = {"u": 0, "d": 0, "r": 1, "l": -1}
 
     # Initialize mapper to change y position depending on the direction
-    new_y_delta = {
-        "u": -1,
-        "d": 1,
-        "r": 0,
-        "l": 0
-    }
-
-
-    # THIS NEEDS TO BE ADJUSTED!
+    new_y_delta = {"u": -1, "d": 1, "r": 0, "l": 0}
 
     # Get the length of the sides of the cube
     dim = 10e12
-    for i in range(1, n_rows-1):
-        dim_cand = len([j for j in range(1, n_cols-1) if map[i][j] != " "])
+    for i in range(1, n_rows - 1):
+        dim_cand = len([j for j in range(1, n_cols - 1) if map[i][j] != " "])
         if dim_cand < dim:
             dim = dim_cand
-    for j in range(1, n_cols-1):
-        dim_cand = len([i for i in range(1, n_rows-1) if map[i][j] != " "])
+    for j in range(1, n_cols - 1):
+        dim_cand = len([i for i in range(1, n_rows - 1) if map[i][j] != " "])
         if dim_cand < dim:
             dim = dim_cand
-    
-    print("dim: {}".format(dim))
 
-    # Get a list of the outer sides of the cube
+    # Mapper from current position to new position and new direction
+    mapper_dict = dict()
+    v1 = [(1, j) for j in range(1 + dim, 1 + dim * 2)]
+    v2 = [(i, 1) for i in range(1 + dim * 3, 1 + dim * 4)]
+    for i in range(dim):
+        mapper_dict[v1[i]] = {"new_pt": v2[i], "new_d": "r"}
+        mapper_dict[v2[i]] = {"new_pt": v1[i], "new_d": "d"}
 
-    # Get an auxilary dictionary that gives us the leftmost and rightmost x positions
-    new_x_wrapping = dict()
-    for i in range(1, n_rows-1):
-        non_space_idx = [j for j in range(1, n_cols-1) if map[i][j] != " "]
-        new_x_wrapping[i] = {"r": non_space_idx[0], "l": non_space_idx[-1]}
+    v3 = [(i, dim + 1) for i in range(1, 1 + dim)]
+    v4 = list(reversed([(i, 1) for i in range(1 + dim * 2, 1 + dim * 3)]))
+    for i in range(dim):
+        mapper_dict[v3[i]] = {"new_pt": v4[i], "new_d": "r"}
+        mapper_dict[v4[i]] = {"new_pt": v3[i], "new_d": "r"}
 
-    # Get an auxilary dictionary that gives us the topmost and bottommost y positions
-    new_y_wrapping = dict()
-    for j in range(1, n_cols-1):
-        non_space_idx = [i for i in range(1, n_rows-1) if map[i][j] != " "]
-        new_y_wrapping[j] = {"d": non_space_idx[0], "u": non_space_idx[-1]}
+    v5 = [(i, dim + 1) for i in range(1 + dim, 1 + dim * 2)]
+    v6 = [(dim * 2 + 1, j) for j in range(1, 1 + dim)]
+    for i in range(dim):
+        mapper_dict[v5[i]] = {"new_pt": v6[i], "new_d": "d"}
+        mapper_dict[v6[i]] = {"new_pt": v5[i], "new_d": "r"}
+
+    v7 = [(1, j) for j in range(1 + dim * 2, 1 + dim * 3)]
+    v8 = [(dim * 4, j) for j in range(1, 1 + dim)]
+    for i in range(dim):
+        mapper_dict[v7[i]] = {"new_pt": v8[i], "new_d": "u"}
+        mapper_dict[v8[i]] = {"new_pt": v7[i], "new_d": "d"}
+
+    v9 = [(dim * 3, j) for j in range(1 + dim, 1 + dim * 2)]
+    v10 = [(i, dim) for i in range(1 + dim * 3, 1 + dim * 4)]
+    for i in range(dim):
+        mapper_dict[v9[i]] = {"new_pt": v10[i], "new_d": "l"}
+        mapper_dict[v10[i]] = {"new_pt": v9[i], "new_d": "u"}
+
+    v11 = [(dim, j) for j in range(1 + dim * 2, 1 + dim * 3)]
+    v12 = [(i, dim * 2) for i in range(1 + dim, 1 + dim * 2)]
+    for i in range(dim):
+        mapper_dict[v11[i]] = {"new_pt": v12[i], "new_d": "l"}
+        mapper_dict[v12[i]] = {"new_pt": v11[i], "new_d": "u"}
+
+    v13 = [(i, dim * 3) for i in range(1, 1 + dim)]
+    v14 = list(reversed([(i, dim * 2) for i in range(1 + dim * 2, 1 + dim * 3)]))
+    for i in range(dim):
+        mapper_dict[v13[i]] = {"new_pt": v14[i], "new_d": "l"}
+        mapper_dict[v14[i]] = {"new_pt": v13[i], "new_d": "l"}
 
     # Initialize the x- and y-indices and direction at the beginning
     y = 1
-    x = new_x_wrapping[1]["r"] # first non-space element
+    x = [j for j in range(1, n_cols - 1) if map[1][j] != " "][
+        0
+    ]  # first non-space element
     d = "r"
+
+    # print(v9)
+    for i, v in enumerate([v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12]):
+        print(i + 1)
+        print("".join([map[i][j] for (i, j) in v]))
 
     # Traverse the map as specified by the instructions
     for inst in instructions:
@@ -111,23 +132,24 @@ def solution(map: List[List[str]], instructions: List[Union[str, int]], verbose:
             if verbose:
                 print("\nMoving {} for {} steps:".format(d, inst))
             for _ in range(inst):
-                
+
                 # Get new candidate position
                 x_cand = x + new_x_delta[d]
                 y_cand = y + new_y_delta[d]
 
-                # Check if we need to wrap around
+                # Wrap around and change direction if needed
                 if map[y_cand][x_cand] == " ":
-                    if d in ["r", "l"]:
-                        x_cand = new_x_wrapping[y][d]
-                    elif d in ["u", "d"]:
-                        y_cand = new_y_wrapping[x][d]
-
-                # Move only if the new position is not blocked with an "#"
-                if map[y_cand][x_cand] != "#":
+                    y_cand, x_cand = mapper_dict[(y, x)]["new_pt"]
+                    d_cand = mapper_dict[(y, x)]["new_d"]
+                    if map[y_cand][x_cand] != "#":
+                        y = y_cand
+                        x = x_cand
+                        d = d_cand
+                # Else don't wrap around and take a step if we can
+                elif map[y_cand][x_cand] != "#":
                     y = y_cand
                     x = x_cand
-                
+
                 if verbose:
                     print("y: {}, x: {}".format(y, x))
 
@@ -136,25 +158,10 @@ def solution(map: List[List[str]], instructions: List[Union[str, int]], verbose:
     result = 1000 * y + 4 * x + d_constant[d]
     return result
 
+
 if __name__ == "__main__":
 
-    # filepath = pathlib.Path("22/input.txt")
-    # map = get_map(filepath)
-    # for line in map:
-    #     print(" ".join([val for val in line]))
-    #instructions = get_instructions(filepath)
-    #print(solution(map, instructions)) # correct: 144012
-
-    # Test 1
-    filepath = pathlib.Path("22/input_text.txt")
+    filepath = pathlib.Path("22/input.txt")
     map = get_map(filepath)
     instructions = get_instructions(filepath)
-    print(solution(map, instructions))
-    for line in map:
-        print(" ".join([val for val in line]))
-
-    # instructions = get_instructions(filepath)
-    # solution(map, instructions, verbose = True)
-    # expected = 5031
-    # assert solution(map, instructions) == expected
-
+    print(solution(map, instructions))  # correct: 144012
