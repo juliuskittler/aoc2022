7c119712be201cfafbd7f6c8430aadf79c112943
juliuskittler <kittler.julius.uni@gmail.com>
2022-12-26 12:54:01 +0100

    Refactor code for 12

--- 12/solution_2.py
+++ 12/solution_2.py
@@ -1,7 +1,9 @@
 import copy
 import pathlib
 import string
-from typing import Dict, List, Optional, Tuple
+from typing import List, Tuple
+
+from utils import BreadthFirstSearch, get_distance
 
 """
 Note: 
@@ -10,32 +12,6 @@ Note:
 possible path from E to the respective "a" node.
 - The "a" node that we choose is simply the first "a" node from our "prev" array.
 """
-
-
-def get_distance(letter_next: str, letter_curr: str) -> int:
-    """Auxilary function to get the distance between 2 letters.
-
-    Here the distance is defined as the number of 'letter steps'
-    you have to take in order to move from one letter to another.
-    E.g. from 'a' to 'b', it's one step (1), from 'b' to 'a' it's
-    one step back (-1), from 'a' to 'c' it's two steps (2), from
-    'c' to 'a' it's two steps back (-2) etc.
-    """
-    # E has the elevation of z, S has the elevation of a
-    if letter_next == "E":
-        letter_next = "z"
-    elif letter_next == "S":
-        letter_next = "a"
-    if letter_curr == "S":
-        letter_curr = "a"
-    elif letter_curr == "E":
-        letter_curr = "z"
-
-    # Compute the distance
-    letter_idx_next = string.ascii_lowercase.index(letter_next)
-    letter_idx_curr = string.ascii_lowercase.index(letter_curr)
-    distance = letter_idx_next - letter_idx_curr
-    return distance
 
 
 def get_edge_coords(
@@ -67,49 +43,6 @@ def get_edge_coords(
     return edge_coords
 
 
-def bfs_solve(
-    start_node: Tuple[int], graph: Dict[Tuple[int], List[Tuple[int]]]
-) -> dict:
-    # Initialize useful variables
-    queue = [start_node]
-    visited = {node: False for node in graph.keys()}
-    prev = {
-        node: None for node in graph.keys()
-    }  # will help us reconstruct the shortest path
-    iter = 0
-
-    # Traverse the graph until all nodes are visited
-    while len(queue) > 0:
-        # Remove the first element from the queu
-        node = queue[0]
-        queue = queue[1:]
-
-        # Get the neighbors of the current node
-        neighbors = graph[node]
-
-        # Loop over each unvisited node
-        for neighbor in neighbors:
-            if not visited[neighbor]:
-                queue.append(neighbor)
-                visited[neighbor] = True
-
-                prev[neighbor] = {"iter": iter, "node": node}
-                iter += 1
-
-    return prev
-
-
-def bfs_reconstruct(start_node: Tuple[int], end_node: Tuple[int], prev: dict):
-    # Reconstruct path going backwards from end node
-    current_node = end_node
-    path = [current_node]
-    while current_node is not start_node:
-        current_node = prev[current_node]["node"]
-        path.append(current_node)
-    path.reverse()
-    return path
-
-
 def solution(heightmap: List[str]) -> int:
     heightmap = copy.deepcopy(heightmap)
 
@@ -134,20 +67,21 @@ def solution(heightmap: List[str]) -> int:
             graph[node] = get_edge_coords(node, heightmap, m, n)
 
     # Conduct BFS
-    prev = bfs_solve(e_coord, graph)
+    bfs = BreadthFirstSearch(graph)
+    bfs.solve(e_coord)
 
     # Identify the "a" node that was visited first
     a_candidates = dict()
     for node in graph:
         i, j = node
-        if heightmap[i][j] == "a" and prev[node] is not None:
-            a_candidates[prev[node]["iter"]] = node
+        if heightmap[i][j] == "a" and bfs.previous[node] is not None:
+            a_candidates[bfs.iter[node]] = node
 
     first_a_node_iter = min(a_candidates.keys())
     first_a_node = a_candidates[first_a_node_iter]
 
     # Reconstruct the path to this "a" node
-    path = bfs_reconstruct(e_coord, first_a_node, prev)
+    path = bfs.reconstruct_path(e_coord, first_a_node)
 
     # Compute the minimum number of steps as the length of the path minus 1
     # (minus 1 because we don't count the start step itself)
